79e625f5e6334242218a4a5a4b8ebd8ba9df3dee
ahmedaadel <60927685+ahmedaadel@users.noreply.github.com>
2024-10-11 16:57:02 +0300

    Dawly: added the menu item component and missing wheel list view

---
+++ app/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:app/Cubit/AppStates.dart';
+import 'package:app/Cubit/Cubit.dart';
+import 'package:app/Modules/MenuItems.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'Network/Remote/Dio_Helper.dart';
+
+void main() {
+  DioHelper.initDio();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(BlocProvider(
+    create: (context)=> AppCubit(),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'POS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyApiScreen(),
+    );
+  }
+}
+
+class MyApiScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AppCubit, AppStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          var cub = AppCubit.get(context);
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('API Integration'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      cub.getAllDataFromAPI("sizes");
+                    },
+                    child: Text('Get Data from API'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Map<String, dynamic> data = {"name":"مقاولات"};
+                      cub.sendDataToAPI("industry", data);
+                    },
+                    child: Text('Send Data to API'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
